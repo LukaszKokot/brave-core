f435560c7c0e4d1d1fb5ab89c2f3b0c21872d4cc
Anton Lazarev <antonok35@gmail.com>
2026-01-05 18:13:05 -0800

    propagate filter list metadata to filters providers

--- components/brave_shields/core/browser/ad_block_filters_provider_manager.cc
+++ components/brave_shields/core/browser/ad_block_filters_provider_manager.cc
@@ -15,19 +15,25 @@
 #include "base/rand_util.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/trace_event/trace_event.h"
+#include "brave/components/brave_shields/core/common/adblock/rs/src/lib.rs.h"
 #include "brave/components/services/brave_shields/mojom/filter_set.mojom-forward.h"
 #include "brave/components/services/brave_shields/mojom/filter_set.mojom.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 namespace {
 
-// Translates the 2-argument `LoadFilters` callback into one that takes a single
+// Translates the 3-argument `LoadFilters` callback into one that takes a single
 // tuple argument. Intended for compatibility with `BarrierCallback`.
-void LoadFiltersTuple(base::RepeatingCallback<void(
-                          std::tuple<std::vector<unsigned char>, uint8_t>)> cb,
-                      std::vector<unsigned char> filter_buffer,
-                      uint8_t permission_mask) {
-  cb.Run(std::tuple(filter_buffer, permission_mask));
+void LoadFiltersTuple(
+    base::RepeatingCallback<void(
+        std::tuple<std::vector<unsigned char>,
+                   uint8_t,
+                   base::OnceCallback<void(adblock::FilterListMetadata)>>)> cb,
+    std::vector<unsigned char> filter_buffer,
+    uint8_t permission_mask,
+    base::OnceCallback<void(adblock::FilterListMetadata)> on_metadata) {
+  cb.Run(std::tuple(std::move(filter_buffer), permission_mask,
+                    std::move(on_metadata)));
 }
 
 }  // namespace
@@ -80,8 +86,10 @@ void AdBlockFiltersProviderManager::OnChanged(bool is_for_default_engine) {
 
 // Use LoadDATBufferForEngine instead, for Filter Provider Manager.
 void AdBlockFiltersProviderManager::LoadFilters(
-    base::OnceCallback<void(std::vector<unsigned char> filter_buffer,
-                            uint8_t permission_mask)>) {
+    base::OnceCallback<void(
+        std::vector<unsigned char> filter_buffer,
+        uint8_t permission_mask,
+        base::OnceCallback<void(adblock::FilterListMetadata)> on_metadata)>) {
   NOTREACHED();
 }
 
@@ -97,11 +105,12 @@ void AdBlockFiltersProviderManager::LoadFiltersForEngine(
   auto& filters_providers = is_for_default_engine
                                 ? default_engine_filters_providers_
                                 : additional_engine_filters_providers_;
-  auto collect_and_merge =
-      base::BarrierCallback<std::tuple<std::vector<unsigned char>, uint8_t>>(
-          filters_providers.size(),
-          base::BindOnce(&AdBlockFiltersProviderManager::FinishCombinating,
-                         weak_factory_.GetWeakPtr(), std::move(cb), flow_id));
+  auto collect_and_merge = base::BarrierCallback<
+      std::tuple<std::vector<unsigned char>, uint8_t,
+                 base::OnceCallback<void(adblock::FilterListMetadata)>>>(
+      filters_providers.size(),
+      base::BindOnce(&AdBlockFiltersProviderManager::FinishCombinating,
+                     weak_factory_.GetWeakPtr(), std::move(cb), flow_id));
   for (auto* const provider : filters_providers) {
     task_tracker_.PostTask(
         base::SequencedTaskRunner::GetCurrentDefault().get(), FROM_HERE,
@@ -115,20 +124,59 @@ void AdBlockFiltersProviderManager::FinishCombinating(
     base::OnceCallback<
         void(const std::vector<unsigned char> verified_engine_dat)> cb,
     uint64_t flow_id,
-    std::vector<std::tuple<std::vector<unsigned char>, uint8_t>> results) {
+    std::vector<
+        std::tuple<std::vector<unsigned char>,
+                   uint8_t,
+                   base::OnceCallback<void(adblock::FilterListMetadata)>>>
+        results) {
   TRACE_EVENT("brave.adblock",
               "AdBlockFiltersProviderManager::FinishCombinating",
               perfetto::TerminatingFlow::ProcessScoped(flow_id));
 
   std::vector<filter_set::mojom::FilterListInputPtr> inputs;
-  std::transform(results.cbegin(), results.cend(), std::back_inserter(inputs),
-                 [](std::tuple<std::vector<unsigned char>, uint8_t> tuple) {
-                   auto r = filter_set::mojom::FilterListInput::New();
-                   r->filters = std::get<0>(tuple);
-                   r->permission_mask = std::get<1>(tuple);
-                   return r;
-                 });
-  filter_set_service_->ParseFilters(std::move(inputs), std::move(cb));
+  inputs.reserve(results.size());
+  std::vector<base::OnceCallback<void(adblock::FilterListMetadata)>>
+      on_metadata_cbs;
+  on_metadata_cbs.reserve(results.size());
+  for (auto& [filters, permission_mask, on_metadata_cb] : results) {
+    auto r = filter_set::mojom::FilterListInput::New();
+    r->filters = std::move(filters);
+    r->permission_mask = permission_mask;
+    inputs.push_back(std::move(r));
+    on_metadata_cbs.push_back(std::move(on_metadata_cb));
+  }
+  filter_set_service_->ParseFilters(
+      std::move(inputs),
+      base::BindOnce(&AdBlockFiltersProviderManager::OnParseFilters,
+                     weak_factory_.GetWeakPtr(), std::move(cb),
+                     std::move(on_metadata_cbs)));
+}
+
+void AdBlockFiltersProviderManager::OnParseFilters(
+    base::OnceCallback<
+        void(const std::vector<unsigned char> verified_engine_dat)> cb,
+    std::vector<base::OnceCallback<void(adblock::FilterListMetadata)>>
+        on_metadata_cbs,
+    const std::vector<unsigned char>& verified_engine_dat,
+    const std::vector<filter_set::mojom::FilterListMetadataPtr> metadata) {
+  CHECK_EQ(on_metadata_cbs.size(), metadata.size());
+  for (unsigned int i = 0; i < metadata.size(); i++) {
+    auto adblock_metadata = adblock::FilterListMetadata();
+    if (metadata[i]->title) {
+      adblock_metadata.title =
+          adblock::OptionalString(true, *metadata[i]->title);
+    }
+    if (metadata[i]->homepage) {
+      adblock_metadata.homepage =
+          adblock::OptionalString(true, *metadata[i]->homepage);
+    }
+    if (metadata[i]->expires_hours) {
+      adblock_metadata.expires_hours =
+          adblock::OptionalU16(true, *metadata[i]->expires_hours);
+    }
+    std::move(on_metadata_cbs[i]).Run(adblock_metadata);
+  }
+  std::move(cb).Run(verified_engine_dat);
 }
 
 }  // namespace brave_shields
