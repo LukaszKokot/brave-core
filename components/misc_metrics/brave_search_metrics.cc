c372d4387a685e9775094f920b3b4bb53dbcfaa1
Darnell Andries <dandries@brave.com>
2026-02-13 14:11:29 -0800

    Include private queries in search metrics

--- components/misc_metrics/brave_search_metrics.cc
+++ components/misc_metrics/brave_search_metrics.cc
@@ -101,7 +101,7 @@ void BraveSearchMetrics::MaybeRecordBraveQuery(const GURL& previous_url,
 }
 
 void BraveSearchMetrics::MaybeRecordOmniboxQuery(const GURL& destination_url,
-                                                  bool is_suggestion) {
+                                                 bool is_suggestion) {
   if (!IsBraveSearchURL(destination_url)) {
     return;
   }
@@ -113,8 +113,9 @@ void BraveSearchMetrics::MaybeRecordOmniboxQuery(const GURL& destination_url,
 
 void BraveSearchMetrics::MaybeRecordNTPSearch(int64_t engine_prepopulate_id) {
   if (engine_prepopulate_id !=
-      static_cast<int64_t>(TemplateURLPrepopulateData::BravePrepopulatedEngineID::
-          PREPOPULATED_ENGINE_ID_BRAVE)) {
+      static_cast<int64_t>(
+          TemplateURLPrepopulateData::BravePrepopulatedEngineID::
+              PREPOPULATED_ENGINE_ID_BRAVE)) {
     return;
   }
   IncrementDictCount(kNTPSearchCountKey);
