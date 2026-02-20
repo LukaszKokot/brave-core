a213c7699fd1d9105ad7396eca2ae3221b7ce0ec
Douglas Daniel <douglashdaniel@gmail.com>
2026-02-19 18:07:18 -0600

    [Wallet]: Add Wallet to Sidebar

--- browser/ui/sidebar/sidebar_utils.cc
+++ browser/ui/sidebar/sidebar_utils.cc
@@ -16,6 +16,7 @@
 #include "brave/browser/ui/sidebar/sidebar_service_factory.h"
 #include "brave/components/ai_chat/core/common/buildflags/buildflags.h"
 #include "brave/components/brave_talk/buildflags/buildflags.h"
+#include "brave/components/brave_wallet/common/buildflags/buildflags.h"
 #include "brave/components/constants/brave_switches.h"
 #include "brave/components/constants/webui_url_constants.h"
 #include "brave/components/sidebar/browser/constants.h"
@@ -149,8 +150,13 @@ SidePanelEntryId SidePanelIdFromSideBarItemType(BuiltInItemType type) {
     case BuiltInItemType::kChatUI:
       return SidePanelEntryId::kChatUI;
 #endif
+#if BUILDFLAG(ENABLE_BRAVE_WALLET)
+    case BuiltInItemType::kWallet:
+      return SidePanelEntryId::kWallet;
+#else
     case BuiltInItemType::kWallet:
       [[fallthrough]];
+#endif
 #if BUILDFLAG(ENABLE_BRAVE_TALK)
     case BuiltInItemType::kBraveTalk:
       [[fallthrough]];
@@ -174,6 +180,10 @@ std::optional<BuiltInItemType> BuiltInItemTypeFromSidePanelId(
       return BuiltInItemType::kBookmarks;
     case SidePanelEntryId::kPlaylist:
       return BuiltInItemType::kPlaylist;
+#if BUILDFLAG(ENABLE_BRAVE_WALLET)
+    case SidePanelEntryId::kWallet:
+      return BuiltInItemType::kWallet;
+#endif
 #if BUILDFLAG(ENABLE_AI_CHAT)
     case SidePanelEntryId::kChatUI:
       return BuiltInItemType::kChatUI;
@@ -223,6 +233,11 @@ void SetLastUsedSidePanel(PrefService* prefs,
       case SidePanelEntryId::kPlaylist:
         type = BuiltInItemType::kPlaylist;
         break;
+#if BUILDFLAG(ENABLE_BRAVE_WALLET)
+      case SidePanelEntryId::kWallet:
+        type = BuiltInItemType::kWallet;
+        break;
+#endif
 #if BUILDFLAG(ENABLE_AI_CHAT)
       case SidePanelEntryId::kChatUI:
         type = BuiltInItemType::kChatUI;
--- components/sidebar/browser/sidebar_service.cc
+++ components/sidebar/browser/sidebar_service.cc
@@ -44,6 +44,7 @@
 
 #if BUILDFLAG(ENABLE_BRAVE_WALLET)
 #include "brave/components/brave_wallet/browser/brave_wallet_utils.h"
+#include "brave/components/brave_wallet/common/features.h"
 #endif  // BUILDFLAG(ENABLE_BRAVE_WALLET)
 
 #if BUILDFLAG(ENABLE_AI_CHAT)
@@ -625,8 +626,16 @@ SidebarItem SidebarService::GetBuiltInItemForType(
     case SidebarItem::BuiltInItemType::kWallet: {
 #if BUILDFLAG(ENABLE_BRAVE_WALLET)
       if (brave_wallet::IsAllowed(prefs_)) {
+        if (base::FeatureList::IsEnabled(
+                brave_wallet::features::kBraveWalletSidePanel)) {
+          return SidebarItem::Create(
+              l10n_util::GetStringUTF16(IDS_SIDEBAR_WALLET_ITEM_TITLE),
+              SidebarItem::Type::kTypeBuiltIn,
+              SidebarItem::BuiltInItemType::kWallet,
+              /* open_in_panel = */ true);
+        }
         return SidebarItem::Create(
-            GURL("chrome://wallet/"),
+            GURL(kBraveUIWalletPageURL),
             l10n_util::GetStringUTF16(IDS_SIDEBAR_WALLET_ITEM_TITLE),
             SidebarItem::Type::kTypeBuiltIn,
             SidebarItem::BuiltInItemType::kWallet,
