7294d60a6294f1ebbc6e2efe80dd86055e471b93
Emerick Rogul <erogul@brave.com>
2026-02-23 16:25:35 -0500

    fixup! [cr146][ios] `FeatureVariation` param now uses a span

--- ios/browser/flags/about_flags.mm
+++ ios/browser/flags/about_flags.mm
@@ -51,12 +51,9 @@ const flags_ui::FeatureEntry::FeatureParam kBraveIOSUserAgentSuffixComment[] = {
     {"default_user_agent", "3"}};
 const flags_ui::FeatureEntry::FeatureVariation
     kBraveIOSUserAgentFeatureVariations[] = {
-        {"- Version", kBraveIOSUserAgentVersion,
-         std::size(kBraveIOSUserAgentVersion), nullptr},
-        {"- Suffix", kBraveIOSUserAgentSuffix,
-         std::size(kBraveIOSUserAgentSuffix), nullptr},
-        {"- Suffix Comment", kBraveIOSUserAgentSuffixComment,
-         std::size(kBraveIOSUserAgentSuffixComment), nullptr}};
+        {"- Version", kBraveIOSUserAgentVersion, nullptr},
+        {"- Suffix", kBraveIOSUserAgentSuffix, nullptr},
+        {"- Suffix Comment", kBraveIOSUserAgentSuffixComment, nullptr}};
 
 #define BRAVE_SKU_SDK_FEATURE_ENTRIES                   \
   EXPAND_FEATURE_ENTRIES({                              \
