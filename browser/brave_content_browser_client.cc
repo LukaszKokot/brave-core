left-TTC/kilo-core

Add CandleEmbeddingGemmaUI for WASM execution
Add the untrusted WebUI controller that hosts the candle embedding
gemma WASM bridge page and registers it with the browser's mojo
interface binder.

browser/brave_content_browser_client.cc
@@ -39,6 +39,7 @@
 #include "brave/browser/profiles/brave_renderer_updater_factory.h"
 #include "brave/browser/skus/skus_service_factory.h"
 #include "brave/browser/ui/brave_ui_features.h"
+#include "brave/browser/ui/webui/candle_wasm/candle_embedding_gemma_ui.h"
 #include "brave/browser/ui/webui/skus_internals_ui.h"
 #include "brave/browser/updater/buildflags.h"
 #include "brave/browser/url_sanitizer/url_sanitizer_service_factory.h"
@@ -81,6 +82,7 @@
 #include "brave/components/global_privacy_control/global_privacy_control_utils.h"
 #include "brave/components/google_sign_in_permission/google_sign_in_permission_throttle.h"
 #include "brave/components/google_sign_in_permission/google_sign_in_permission_util.h"
+#include "brave/components/local_ai/common/candle.mojom.h"
 #include "brave/components/ntp_background_images/browser/mojom/ntp_background_images.mojom.h"
 #include "brave/components/password_strength_meter/password_strength_meter.mojom.h"
 #include "brave/components/playlist/content/browser/playlist_background_web_contents_helper.h"
@@ -966,6 +968,9 @@ void BraveContentBrowserClient::RegisterBrowserInterfaceBindersForFrame(
 
   map->Add<skus::mojom::SkusService>(
       base::BindRepeating(&MaybeBindSkusSdkImpl));
+  content::RegisterWebUIControllerInterfaceBinder<
+      local_ai::mojom::CandleService,
+      local_ai::UntrustedCandleEmbeddingGemmaUI>(map);
 #if BUILDFLAG(ENABLE_BRAVE_VPN)
   map->Add<brave_vpn::mojom::ServiceHandler>(
       base::BindRepeating(&MaybeBindBraveVpnImpl));

browser/ui/webui/candle_wasm/candle_embedding_gemma_ui.h
@@ -1,0 +1,46 @@
+// Copyright (c) 2025 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_BROWSER_UI_WEBUI_CANDLE_WASM_CANDLE_EMBEDDING_GEMMA_UI_H_
+#define BRAVE_BROWSER_UI_WEBUI_CANDLE_WASM_CANDLE_EMBEDDING_GEMMA_UI_H_
+
+#include "brave/components/local_ai/common/candle.mojom.h"
+#include "content/public/browser/web_ui_controller.h"
+#include "content/public/browser/webui_config.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "ui/webui/mojo_web_ui_controller.h"
+
+namespace local_ai {
+
+// We need to run embedding gemma WASM in an isolated context because we might
+// provide untrusted input to be embedded. ex. passages of page contents.
+class UntrustedCandleEmbeddingGemmaUI : public ui::MojoWebUIController {
+ public:
+  explicit UntrustedCandleEmbeddingGemmaUI(content::WebUI* web_ui);
+  UntrustedCandleEmbeddingGemmaUI(const UntrustedCandleEmbeddingGemmaUI&) =
+      delete;
+  UntrustedCandleEmbeddingGemmaUI& operator=(
+      const UntrustedCandleEmbeddingGemmaUI&) = delete;
+  ~UntrustedCandleEmbeddingGemmaUI() override;
+
+  void BindInterface(mojo::PendingReceiver<mojom::CandleService> receiver);
+
+ private:
+  WEB_UI_CONTROLLER_TYPE_DECL();
+};
+
+class UntrustedCandleEmbeddingGemmaUIConfig : public content::WebUIConfig {
+ public:
+  UntrustedCandleEmbeddingGemmaUIConfig();
+  ~UntrustedCandleEmbeddingGemmaUIConfig() override = default;
+
+  std::unique_ptr<content::WebUIController> CreateWebUIController(
+      content::WebUI* web_ui,
+      const GURL& url) override;
+};
+
+}  // namespace local_ai
+
+#endif  // BRAVE_BROWSER_UI_WEBUI_CANDLE_WASM_CANDLE_EMBEDDING_GEMMA_UI_H_

LukaszKokot/brave-core

browser/brave_content_browser_client.cc
@@ -39,6 +39,7 @@
 #include "brave/browser/profiles/brave_renderer_updater_factory.h"
 #include "brave/browser/skus/skus_service_factory.h"
 #include "brave/browser/ui/brave_ui_features.h"
+#include "brave/browser/ui/webui/candle_wasm/candle_embedding_gemma_ui.h"
 #include "brave/browser/ui/webui/skus_internals_ui.h"
 #include "brave/browser/updater/buildflags.h"
 #include "brave/browser/url_sanitizer/url_sanitizer_service_factory.h"
@@ -81,6 +82,7 @@
 #include "brave/components/global_privacy_control/global_privacy_control_utils.h"
 #include "brave/components/google_sign_in_permission/google_sign_in_permission_throttle.h"
 #include "brave/components/google_sign_in_permission/google_sign_in_permission_util.h"
+#include "brave/components/local_ai/core/local_ai.mojom.h"
 #include "brave/components/ntp_background_images/browser/mojom/ntp_background_images.mojom.h"
 #include "brave/components/password_strength_meter/password_strength_meter.mojom.h"
 #include "brave/components/playlist/content/browser/playlist_background_web_contents_helper.h"
@@ -966,6 +968,9 @@ void BraveContentBrowserClient::RegisterBrowserInterfaceBindersForFrame(
 
   map->Add<skus::mojom::SkusService>(
       base::BindRepeating(&MaybeBindSkusSdkImpl));
+  content::RegisterWebUIControllerInterfaceBinder<
+      local_ai::mojom::LocalAIService,
+      local_ai::UntrustedCandleEmbeddingGemmaUI>(map);
 #if BUILDFLAG(ENABLE_BRAVE_VPN)
   map->Add<brave_vpn::mojom::ServiceHandler>(
       base::BindRepeating(&MaybeBindBraveVpnImpl));

browser/ui/webui/candle_wasm/candle_embedding_gemma_ui.h
@@ -1,0 +1,46 @@
+// Copyright (c) 2026 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_BROWSER_UI_WEBUI_CANDLE_WASM_CANDLE_EMBEDDING_GEMMA_UI_H_
+#define BRAVE_BROWSER_UI_WEBUI_CANDLE_WASM_CANDLE_EMBEDDING_GEMMA_UI_H_
+
+#include "brave/components/local_ai/core/local_ai.mojom.h"
+#include "content/public/browser/web_ui_controller.h"
+#include "content/public/browser/webui_config.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "ui/webui/mojo_web_ui_controller.h"
+
+namespace local_ai {
+
+// We need to run embedding gemma WASM in an isolated context because we might
+// provide untrusted input to be embedded. ex. passages of page contents.
+class UntrustedCandleEmbeddingGemmaUI : public ui::MojoWebUIController {
+ public:
+  explicit UntrustedCandleEmbeddingGemmaUI(content::WebUI* web_ui);
+  UntrustedCandleEmbeddingGemmaUI(const UntrustedCandleEmbeddingGemmaUI&) =
+      delete;
+  UntrustedCandleEmbeddingGemmaUI& operator=(
+      const UntrustedCandleEmbeddingGemmaUI&) = delete;
+  ~UntrustedCandleEmbeddingGemmaUI() override;
+
+  void BindInterface(mojo::PendingReceiver<mojom::LocalAIService> receiver);
+
+ private:
+  WEB_UI_CONTROLLER_TYPE_DECL();
+};
+
+class UntrustedCandleEmbeddingGemmaUIConfig : public content::WebUIConfig {
+ public:
+  UntrustedCandleEmbeddingGemmaUIConfig();
+  ~UntrustedCandleEmbeddingGemmaUIConfig() override = default;
+
+  std::unique_ptr<content::WebUIController> CreateWebUIController(
+      content::WebUI* web_ui,
+      const GURL& url) override;
+};
+
+}  // namespace local_ai
+
+#endif  // BRAVE_BROWSER_UI_WEBUI_CANDLE_WASM_CANDLE_EMBEDDING_GEMMA_UI_H_